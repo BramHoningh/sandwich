681471d571c13235a137b6bd2dbd93044a7949d8
Bram - Pixeldenker <bram@pixeldenker.nl>
2019-03-03 22:20:38 +0100

    Add design-system page

---
+++ src/views/DesignSystem/DesignSystem.vue
@@ -0,0 +1,9 @@
+<style src="./DesignSystem.scss" lang="scss" scoped></style>
+<script src="./DesignSystem.js"></script>
+
+<template>
+  <div class="design-system">
+    <h1>Heading 01</h1>
+    <h2>Heading 02</h2>
+  </div>
+</template>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,6 +2,5 @@
 
 export default {
   name: 'Home',
-  components: {
-  },
+  components: {},
 };
